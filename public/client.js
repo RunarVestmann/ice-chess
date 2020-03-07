e773e6c8e2fbbd282e1934b36ca7231695734fbf
RunarVestmann <runarvestmann@gmail.com>
2020-03-07 21:27:12 +0000

    initial commit

---
+++ public/client.js
@@ -0,0 +1,45 @@
+const socket = io();
+
+const gameStatus = document.getElementById('game-status');
+
+const handleMove = (from, to) => {
+    socket.emit('move', ({from, to, promotion: 'q'}));
+}
+
+let board;
+
+function updateBoard(game){
+    board.position(game.fen);
+
+    let player;
+    if(game.turn === 'w'){
+        player = 'White';
+        gameStatus.style.color = "gray";
+    }
+    else{
+        player = 'Black';
+        gameStatus.style.color = "black";
+    }
+    if(game.in_checkmate)
+        gameStatus.textContent = "Game over, " + player + ' is in checkmate'; 
+    else if(game.in_draw)
+        gameStatus.textContent = "Game over, it's a draw";
+    else{
+        gameStatus.textContent = `It's ${player}'s turn`; 
+        if(game.in_check)
+            gameStatus.textContent += `, ${player} is in check`;   
+    }
+}
+
+socket.on('start', (game) => {
+    board = Chessboard('board', {
+            position: game.fen,
+            draggable:true,
+            onDrop: handleMove
+    });
+    updateBoard(game);
+});
+
+socket.on('move', (game) => {
+    updateBoard(game);
+});
---
+++ src/index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const { Chess } = require('chess.js');
+
+const game = Chess();
+let whiteId;
+let blackId;
+
+let gameInReset = false;
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+server.listen(port);
+
+io.on('connection', (socket) => {
+
+    socket.emit('start', {
+        fen: game.fen(),
+        turn: game.turn(),
+        in_checkmate: game.in_checkmate(),
+        in_draw: game.in_draw(),
+        in_check: game.in_check()
+    });
+    if(!whiteId)
+        whiteId = socket.id;
+    else if(!blackId)
+        blackId = socket.id;
+
+    socket.on('disconnect', (reason) => {
+        if(socket.id === whiteId)
+            whiteId = '';
+        else if(socket.id === blackId)
+            blackId = '';
+    });
+    
+    socket.on('move', (move) => {
+        const whiteMoving = (socket.id === whiteId) && (game.turn() === 'w');
+        const blackMoving = (socket.id === blackId) && (game.turn() === 'b');
+
+        let gameObj = getGameObject(game);
+
+        if(whiteMoving || blackMoving){
+            const moveObj = game.move({from : move.from, to: move.to, promotion:move.promotion});
+            gameObj = getGameObject(game);
+            if(moveObj !== null)
+                socket.broadcast.emit('move', gameObj);
+        }
+        socket.emit('move', gameObj);  
+
+        if(game.game_over() && !gameInReset){
+            gameInReset = true;
+            setTimeout(() => {
+                game.reset();
+                io.sockets.emit('start', getGameObject(game));
+                gameInReset = false;
+            },10000);
+        }
+    });
+});
+
+function getGameObject(game){
+    return{
+        fen: game.fen(),
+        turn: game.turn(),
+        in_checkmate: game.in_checkmate(),
+        in_draw: game.in_draw(),
+        in_check: game.in_check()
+    };
+}
